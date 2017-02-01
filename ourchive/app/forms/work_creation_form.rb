2ffb6dd69ba87a02ecfed9b08dffa4f4f052f3d9
Elena Palmer <c.elena.palmer@gmail.com>
2017-01-31 21:25:19 -0600

    Work Form Tags
    Working tag fields with string splitting. Tag functionality
    should be moved into a concern.

--- ourchive/app/forms/work_creation_form.rb
+++ ourchive/app/forms/work_creation_form.rb
@@ -10,11 +10,11 @@ class WorkCreationForm
     attribute :total_chapters, Integer
     attribute :is_series, Boolean
     attribute :body_text, String
-    attribute :character_tags, Array[String]
-    attribute :theme_tags, Array[String]
-    attribute :fandom_tags, Array[String]
-    attribute :primary_pairing_tags, Array[String]
-    attribute :secondary_pairing_tags, Array[String]
+    attribute :character_tags, String
+    attribute :theme_tags, String
+    attribute :fandom_tags, String
+    attribute :primary_pairing_tags, String
+    attribute :secondary_pairing_tags, String
 
     validates :work_summary, presence: true
 
@@ -35,8 +35,29 @@ class WorkCreationForm
         @work = Work.create!(work_summary: work_summary, is_complete: is_complete, series_id: series_id, collection_id: collection_id,
             word_count: word_count, total_chapters: total_chapters, is_series: is_series)
         chapter = @work.chapters.create!(body_text: body_text, chapter_summary: work_summary, chapter_number: 1)
-        character_tags.each do |tag|
-            @tag = Tag.where(tag_text: tag).first_or_create
+        character_tags_split = character_tags.to_s.split(', ')
+        character_tags_split.each do |tag|
+            @tag = Tag.where(text: tag, is_character: true).first_or_create
+            WorkTag.create!(tag_id: @tag.id, work_id: @work.id)
+        end
+        theme_tags_split = theme_tags.to_s.split(', ')
+        theme_tags_split.each do |tag|
+            @tag = Tag.where(text: tag, is_theme: true).first_or_create
+            WorkTag.create!(tag_id: @tag.id, work_id: @work.id)
+        end
+        primary_pairing_tags_split = primary_pairing_tags.to_s.split(', ')
+        primary_pairing_tags_split.each do |tag|
+            @tag = Tag.where(text: tag, is_primary_pairing: true).first_or_create
+            WorkTag.create!(tag_id: @tag.id, work_id: @work.id)
+        end
+        secondary_pairing_tags_split = secondary_pairing_tags.to_s.split(', ')
+        secondary_pairing_tags_split.each do |tag|
+            @tag = Tag.where(text: tag, is_secondary_pairing: true).first_or_create
+            WorkTag.create!(tag_id: @tag.id, work_id: @work.id)
+        end
+        fandom_tags_split = fandom_tags.to_s.split(', ')
+        fandom_tags_split.each do |tag|
+            @tag = Tag.where(text: tag, is_fandom: true).first_or_create
             WorkTag.create!(tag_id: @tag.id, work_id: @work.id)
         end
     end
