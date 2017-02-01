cbb0ffdd2b35e19450db8b7c4599563452ccaae3
Elena Palmer <c.elena.palmer@gmail.com>
2017-01-31 21:44:01 -0600

    Works Form WIP
    Switched tags to use newline. Still working on various parts incl
    interface and scaling, but fk associations are working.

--- ourchive/app/forms/work_creation_form.rb
+++ ourchive/app/forms/work_creation_form.rb
@@ -1,6 +1,6 @@
 class WorkCreationForm
     include ActiveModel::Model
-    include Virtus
+    include Virtus.model
 
     attribute :work_summary, String
     attribute :is_complete, Boolean
@@ -35,27 +35,27 @@ class WorkCreationForm
         @work = Work.create!(work_summary: work_summary, is_complete: is_complete, series_id: series_id, collection_id: collection_id,
             word_count: word_count, total_chapters: total_chapters, is_series: is_series)
         chapter = @work.chapters.create!(body_text: body_text, chapter_summary: work_summary, chapter_number: 1)
-        character_tags_split = character_tags.to_s.split(', ')
+        character_tags_split = character_tags.to_s.split("\r\n")
         character_tags_split.each do |tag|
             @tag = Tag.where(text: tag, is_character: true).first_or_create
             WorkTag.create!(tag_id: @tag.id, work_id: @work.id)
         end
-        theme_tags_split = theme_tags.to_s.split(', ')
+        theme_tags_split = theme_tags.to_s.split("\r\n")
         theme_tags_split.each do |tag|
             @tag = Tag.where(text: tag, is_theme: true).first_or_create
             WorkTag.create!(tag_id: @tag.id, work_id: @work.id)
         end
-        primary_pairing_tags_split = primary_pairing_tags.to_s.split(', ')
+        primary_pairing_tags_split = primary_pairing_tags.to_s.split("\r\n")
         primary_pairing_tags_split.each do |tag|
             @tag = Tag.where(text: tag, is_primary_pairing: true).first_or_create
             WorkTag.create!(tag_id: @tag.id, work_id: @work.id)
         end
-        secondary_pairing_tags_split = secondary_pairing_tags.to_s.split(', ')
+        secondary_pairing_tags_split = secondary_pairing_tags.to_s.split("\r\n")
         secondary_pairing_tags_split.each do |tag|
             @tag = Tag.where(text: tag, is_secondary_pairing: true).first_or_create
             WorkTag.create!(tag_id: @tag.id, work_id: @work.id)
         end
-        fandom_tags_split = fandom_tags.to_s.split(', ')
+        fandom_tags_split = fandom_tags.to_s.split("\r\n")
         fandom_tags_split.each do |tag|
             @tag = Tag.where(text: tag, is_fandom: true).first_or_create
             WorkTag.create!(tag_id: @tag.id, work_id: @work.id)
