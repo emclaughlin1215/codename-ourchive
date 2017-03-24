437596682a886b324e64883181e10e2c497ff6a4
cpalmer8 <c.elena.palmer@outlook.com>
2017-03-24 19:23:42 +0000

    Works Javascript Updates
    Coffeescript was borked - replace with plain old jquery/.js file.
    Fix error where accessing works/new from link would disabled javascript.
    (thanks turbolinks)

---
+++ public/assets/works-2c61dfce7917a8b363c43354c7045e31613ad0783cd10feba4c9f076f6352df1.js
@@ -0,0 +1,75 @@
+/*# Place all the behaviors and hooks related to the matching controller here.
+# All this logic will automatically be available in application.js.
+# You can use CoffeeScript in this file: http://coffeescript.org/
+jQuery ->
+	$('#multi_chapter').change ->
+  		if (this.checked)
+        	  $('#chapter_count').show()
+        	  $('#multi_chapter_buttons').show() 
+        	  $('#chapter_textbox').show()
+        	  $('#work_textbox').hide()
+    		else
+        	  $('#chapter_count').hide()
+        	  $('#chapter_textbox').hide()
+        	  $('#work_textbox').show()
+        	  $('#multi_chapter_buttons').hide()
+		return
+jQuery ->
+	$('#is_series').change ->
+  		if (this.checked)
+        	  $('#series_id').show() 
+    		else
+        	  $('#series_id').hide()
+		return
+jQuery ->
+	$('#work_type').change ->
+		work_type = $('#work_type :selected').val()	
+		if (work_type == '1')
+			$('#1_form').show()
+		else
+			$('#1_form').hide() 
+		return
+jQuery ->
+	$('#in_collection').change ->
+  		if (this.checked)
+        	$('#collection_id').show() 
+    	else
+        	$('#collection_id').hide()*/
+
+//<script type="text/javascript">
+    $(function() {
+
+ // Below is the name of the textfield that will be autocomplete    
+    $('#0_tags_filter').autocomplete({
+	    appendTo: '#new_work_creation_form',
+            source: '/tag_suggestions/?type=0',
+            select: function(event, ui) {
+                alert("hi");
+		return false;
+	    }
+    });
+}
+/*jQuery ->
+	$('#0_tags_filter').autocomplete
+           appendTo: '#new_work_creation_form',
+           source: '/tag_suggestions/?type=0',
+	   select: (event, ui) ->
+	     event.preventDefault()
+	     return
+	return*/
+/*jQuery ->
+       $('#1_tags_filter').autocomplete
+           appendTo: '#new_work_creation_form',
+           source: '/tag_suggestions/?type=1'
+jQuery ->
+       $('#2_tags_filter').autocomplete
+           appendTo: '#new_work_creation_form',
+           source: '/tag_suggestions/?type=2'
+jQuery ->
+       $('#3_tags_filter').autocomplete
+           appendTo: '#new_work_creation_form',
+           source: '/tag_suggestions/?type=3'
+jQuery ->
+       $('#4_tags_filter').autocomplete
+           appendTo: '#new_work_creation_form',
+           source: '/tag_suggestions/?type=4' */
---
+++ public/assets/works-aa8e4bfb1a6f9f8ef17dfd42d965bdeb3ba8e0a72c6b5564eab87f1864ea13c5.js
@@ -0,0 +1,90 @@
+(function() {
+  jQuery(function() {
+    $('#multi_chapter').change(function() {
+      if (this.checked) {
+        $('#chapter_count').show();
+        $('#multi_chapter_buttons').show();
+        $('#chapter_textbox').show();
+        return $('#work_textbox').hide();
+      } else {
+        $('#chapter_count').hide();
+        $('#chapter_textbox').hide();
+        $('#work_textbox').show();
+        return $('#multi_chapter_buttons').hide();
+      }
+    });
+  });
+
+  jQuery(function() {
+    $('#is_series').change(function() {
+      if (this.checked) {
+        return $('#series_id').show();
+      } else {
+        return $('#series_id').hide();
+      }
+    });
+  });
+
+  jQuery(function() {
+    return $('#work_type').change(function() {
+      var work_type;
+      work_type = $('#work_type :selected').val();
+      if (work_type === '1') {
+        $('#1_form').show();
+      } else {
+        $('#1_form').hide();
+      }
+    });
+  });
+
+  jQuery(function() {
+    return $('#in_collection').change(function() {
+      if (this.checked) {
+        return $('#collection_id').show();
+      } else {
+        return $('#collection_id').hide();
+      }
+    });
+  });
+
+  jQuery(function() {
+    $('#0_tags_filter').autocomplete({
+      appendTo: '#new_work_creation_form',
+      source: '/tag_suggestions/?type=0'
+    });
+    ({
+      select: function(event, ui) {
+        event.preventDefault();
+      }
+    });
+  });
+
+  jQuery(function() {
+    return $('#1_tags_filter').autocomplete({
+      appendTo: '#new_work_creation_form',
+      source: '/tag_suggestions/?type=1'
+    });
+  });
+
+  jQuery(function() {
+    return $('#2_tags_filter').autocomplete({
+      appendTo: '#new_work_creation_form',
+      source: '/tag_suggestions/?type=2'
+    });
+  });
+
+  jQuery(function() {
+    return $('#3_tags_filter').autocomplete({
+      appendTo: '#new_work_creation_form',
+      source: '/tag_suggestions/?type=3'
+    });
+  });
+
+  jQuery(function() {
+    return $('#4_tags_filter').autocomplete({
+      appendTo: '#new_work_creation_form',
+      source: '/tag_suggestions/?type=4'
+    });
+  });
+
+}).call(this);
