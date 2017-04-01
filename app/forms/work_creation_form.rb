b7c99a5512ea35ad6c34293437c5219fdf5206c5
cpalmer8 <c.elena.palmer@outlook.com>
2017-04-01 16:16:24 +0000

    Bootstrap classes

--- app/forms/work_creation_form.rb
+++ app/forms/work_creation_form.rb
@@ -48,7 +48,7 @@ class WorkCreationForm
         self.user_id = user.id
     end
     def update()
-      Work.update(work_id, :work_summary => work_summary, :title => work_title, :series_id => series_id, :is_complete => is_complete, :collection_id => collection_id,
+      Work.update(work_id, :work_summary => work_summary, :title => work_title, :is_complete => is_complete,
         :word_count => word_count, :total_chapters => total_chapters)
       @work = Work.find(work_id)
       update_tags()
@@ -56,7 +56,7 @@ class WorkCreationForm
     private
 
     def persist!
-        @work = Work.create!(work_summary: work_summary, is_complete: is_complete, series_id: series_id, collection_id: collection_id, 
+        @work = Work.create!(work_summary: work_summary, is_complete: is_complete, series_id: series_id,
             word_count: body_text.split.size, total_chapters: total_chapters, is_series: is_series, user_id: user_id, title: work_title, work_type: work_type)
         chapter = @work.chapters.create!(body_text: body_text, chapter_summary: work_summary, chapter_number: 1, title: work_title)
         counter = 1
@@ -64,7 +64,6 @@ class WorkCreationForm
           @work.chapters.create!(body_text: chapter, chapter_number: counter, title: @chapter_titles[counter - 1])
           counter = counter + 1
         end
-        # todo save chapter list
         one_tags_split = one_tags.to_s.split(",")
         one_tags_split.each do |tag|
             @tag = Tag.where(text: tag, type_key: 1).first_or_create
