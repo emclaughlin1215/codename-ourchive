781ae76d50786543b4916917e101b12c31aebf69
Elena <c.elena.palmer@outlook.com>
2017-04-19 19:28:12 -0500

    Progress on searching

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,2 +1,13 @@
 class SearchController < ApplicationController
+
+def show
+  @results = Work.search(params[:search_term])
+  @title = @results.results.first._source.title
+end
+
+private
+
+  def search_params
+    params.require(:search).permit(:search_term)
+  end
 end
