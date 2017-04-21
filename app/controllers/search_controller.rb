14c1cbed681f7e9c7e0e50c3b9dc4e39ea37ec4c
Elena <c.elena.palmer@outlook.com>
2017-04-20 23:17:52 -0500

    Search tags and work text

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,9 +6,18 @@ def index
 end
 
 def show
-  @results = Work.search(params[:search_term], where: {work_type: params[:work_type]}, fields: [{title: :word_start}, {work_summary: :word_start}])
+  # @results = Searchkick.search(params[:search_term], index_name: [Work, Tag], where: {work_type: params[:work_type]}, fields: [{title: :word_start}, {work_summary: :word_start}, {text: :word_start}])
+  @works = Work.search(params[:search_term], where: {work_type: params[:work_type]}, fields: [{title: :word_start}, {work_summary: :word_start}])
   @tags = Tag.search(params[:search_term], fields: [{text: :word_start}])
-  puts @tags
+  ids = ""
+  counter = 0
+  @tags.each do |t|
+    if (counter > 0)
+      ids += ","
+    end
+    ids += t.id.to_s
+  end
+  @tag_works = Work.joins("LEFT JOIN work_tags on work_tags.work_id = works.id LEFT JOIN tags on tags.id = work_tags.id WHERE tags.id in ("+ ids+")")
 end
 
 private
